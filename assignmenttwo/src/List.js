5bdde1cf5b0f08c87fb0614ce06d0c9d39de5683
kuljit <kuljit>
2019-08-03 22:13:10 +0530

    changes

--- assignmenttwo/src/List.js
+++ assignmenttwo/src/List.js
@@ -1,7 +1,10 @@
 import React, { Component, Fragment } from 'react';
+import Trailer from './Trailer';
 import './List.css';
 
 let rowCount = 0;
+let trailerPerRow = 0;
+
 class List extends Component {
 
 	constructor(props){
@@ -9,18 +12,47 @@ class List extends Component {
 		this.state = {
 			trailerDisplayList : {}
 		}
+		this.trailerContainer = React.createRef();
+	}
+
+	handleWindow = () => {
+		const width = document.documentElement.clientWidth;
+		let newTrailerPerRow = 0;
+		if(width <= 510) {
+			newTrailerPerRow = 2;
+		}else if(width <= 680) {
+			newTrailerPerRow = 3;
+		}else if(width <= 820) {
+			newTrailerPerRow = 4;
+		} else if(width <= 1000){
+			newTrailerPerRow =  5;
+		} else {
+			newTrailerPerRow =  6;
+		}
+		if(newTrailerPerRow !== trailerPerRow){
+			trailerPerRow = newTrailerPerRow;
+			const { trailerList } = this.props;
+			let trailerListCopy = {...trailerList}
+			this.setRowCount(trailerListCopy)
+		}
 	}
 
 	componentDidMount(){
 		const { trailerList } = this.props;
 		let trailerListCopy = {...trailerList}
 		this.setRowCount(trailerListCopy)
+		this.handleWindow();
+		window.addEventListener("resize", this.handleWindow);
+	}
+
+	componentWillUnmount(){
+		window.removeEventListener("resize", this.handleWindow);
 	}
 
 	setRowCount = (trailerList) => {
 		const trailerKeys = Object.keys(trailerList);
 		trailerKeys.forEach((trailer,index) => {
-			const isFirstTrailerOfRow = (index % 6 === 0);
+			const isFirstTrailerOfRow = (index % trailerPerRow === 0);
 			if(isFirstTrailerOfRow) {
 				rowCount++;
 			}
@@ -34,11 +66,17 @@ class List extends Component {
 
 	playTrailerRequest = (trailerKey) => {
 		const { trailerDisplayList } = this.state;
-		console.log(trailerDisplayList[trailerKey].rowCount)
-		console.log(trailerDisplayList[trailerKey].firstOfRow)
 		this.setState({
 			trailerAtRow : trailerDisplayList[trailerKey].rowCount
 		})
+		setTimeout(()=>{
+			if(this.trailerContainer.current) {
+				this.trailerContainer.current.scrollIntoView({
+					behavior: "smooth",
+					block: "start"
+				});
+			}
+		},100)
 	}
 
 	getClassNames = (showTrailer, isFirstOfRow) => {
@@ -63,129 +101,8 @@ class List extends Component {
 						const isFirstOfRow = trailerDisplayList[trailer].firstOfRow;
 						return (
 							<div onClick={() => this.playTrailerRequest(trailer)} key={key} className={this.getClassNames(showTrailer,isFirstOfRow)}>
-								{isFirstOfRow && showTrailer && <div className='trailer-container'>
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									TRAILER TRAILER TRAILER TRAILER
-									
-
+								{isFirstOfRow && showTrailer && <div ref={this.trailerContainer} className='trailer-container'>
+									<Trailer/>
 								</div>}
 								<img className={showTrailer && isFirstOfRow ? 'trailer-image image-placement' : 'trailer-image'} src={`https://in.bmscdn.com/events/moviecard/${trailer}.jpg`}/>
 							</div>
