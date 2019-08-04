dc13351a5c9fbf29a871767f1fb3f78738f528ef
kuljit <kuljit>
2019-08-04 20:42:12 +0530

    changes

--- assignmenttwo/src/common/DisplayDate.js
+++ assignmenttwo/src/common/DisplayDate.js
@@ -6,7 +6,7 @@ export default function Date(props){
     const showDispalyMonth = new moment(props.ShowDate).format('MMM');
     const showDisplayYear = new moment(props.ShowDate).format('YYYY');
     return (
-        <div>
+        <div style={{cursor:'pointer'}}>
             <div className='month'>{showDispalyMonth}</div>
             <div className='year'>{showDisplayYear}</div>
         </div>
